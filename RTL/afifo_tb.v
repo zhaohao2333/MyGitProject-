c3001a2810ff9c246abc7831121d91d0160e142b
zhaohao2333 <1531549264@qq.com>
2020-11-04 23:29:11 +0800

    异步fifo文件提交

---
+++ RTL/afifo_tb.v
@@ -0,0 +1,66 @@
+`timescale 1ns/1ps
+module afifo_tb ();
+    reg                 wclk;
+    reg                 wrst_n;
+    reg                 winc;
+    reg     [7:0]       wdata;
+    wire                wfull;
+    wire                wfull_almost;
+    // read clock domain
+    reg                 rclk;
+    reg                 rrst_n;
+    reg                 rinc;
+    wire    [7:0]       rdata;
+    wire                rempty;
+    wire                rempty_almost;
+
+    // -------------------initial-------------------------- /
+    // define two clocks
+    always #5 wclk = ~ wclk;
+    always #12 rclk = ~ rclk;
+    // 
+    initial begin
+       wclk = 0;
+       rclk = 1;
+       wrst_n = 1;
+       rrst_n = 1;
+       winc = 0;
+       rinc = 0;
+       wdata = 8'b0000_0001;
+       # 8
+       wrst_n = 0;
+       rrst_n = 0;
+       # 30
+       wrst_n = 1;
+       rrst_n = 1;
+       # 100
+       winc = 1;
+       rinc = 1;
+       # 10000
+       $finish;
+    end
+    // -------------------module--------------------------- //
+    async_fifo
+    #(
+    .DEPTH(8),
+    .FIFO_DEEP(256),
+    .WIDTH(4),
+    .GAP(3)
+    )
+    fifo1(
+    // write clock domain
+    .wclk(wclk),
+    .wrst_n(wrst_n),
+    .winc(winc),
+    .wdata(wdata),
+    .wfull(wfull),
+    .wfull_almost(wfull_almost),
+    // read clock domain
+    .rclk(rclk),
+    .rrst_n(rrst_n),
+    .rinc(rinc),
+    .rdata(rdata),
+    .rempty(rempty),
+    .rempty_almost(rempty_almost)
+    );
+endmodule //afifo_tb
---
+++ RTL/async_fifo.v
@@ -0,0 +1,104 @@
+module async_fifo #(
+    parameter DEPTH = 8,
+    parameter FIFO_DEEP = 256,
+    parameter WIDTH = 4,
+    parameter GAP = 3)
+    (
+    // write clock domain
+    input               wclk,
+    input               wrst_n,
+    input               winc,
+    input  [DEPTH-1:0]  wdata,
+    output              wfull,
+    output reg          wfull_almost,
+    // read clock domain
+    input                   rclk,
+    input                   rrst_n,
+    input                   rinc,
+    output reg [DEPTH-1:0]  rdata,
+    output                  rempty,
+    output reg              rempty_almost);
+
+    // **************************************************************** //
+    wire                wen;
+    wire                ren;
+    reg    [WIDTH-1:0]  mem[DEPTH-1:0];
+    reg    [DEPTH  :0]  wptr, r_wptr_g1, r_wptr_g2, wgap;
+    reg    [DEPTH  :0]  rptr, w_rptr_g1, w_rptr_g2, rgap;
+    wire   [DEPTH  :0]  wptr_g, r_wptr;
+    wire   [DEPTH  :0]  rptr_g, w_rptr;
+
+    // **************************************************************** //
+    // ---------------------write logic-------------------------------- //
+
+    assign wen = winc && (~wfull);
+    always @(posedge wclk or negedge wrst_n) begin
+        if(!wrst_n)
+            wptr <= 0;
+        else if(wen) begin
+            wptr <= wptr + 1;
+            mem[wptr[DEPTH-1:0]] <= wdata;
+        end
+    end
+    // bin to gary
+    assign wptr_g = wptr ^ (wptr >> 1);
+    // rptr_g synchronzier controled by wclk 
+    always @(posedge wclk) begin
+        w_rptr_g1 <= rptr_g;
+        w_rptr_g2 <= w_rptr_g1;
+    end
+    // gary to bin
+    assign w_rptr = w_rptr_g2 ^ (w_rptr_g2 >> 1) ^ 
+                    (w_rptr_g2 >> 2) ^ (w_rptr_g2 >> 3);
+    // ---------------------read logic----------------------------------//
+    assign ren = rinc && (~rempty);
+    always @(posedge rclk or negedge rrst_n) begin
+        if(!rrst_n)
+            rptr = 0;
+        else if (wen) begin
+            rptr <= rptr + 1;
+            rdata <= mem[rptr[DEPTH-1:0]];
+        end
+    end
+    // bin to gary
+    assign rptr_g = rptr ^ (rptr >> 1);
+    // wptr_g synchronizer controled by rclk
+    always @(posedge rclk) begin
+        r_wptr_g1 <= wptr_g;
+        r_wptr_g2 <= r_wptr_g1;
+    end
+    // gary to bin 
+    assign r_wptr = r_wptr_g2 ^ (r_wptr_g2 >> 1) ^
+                    (r_wptr_g2 >> 2) ^ (r_wptr_g2 >> 3);
+
+    // --------------------wfull and wfull_almost--------------------- //
+    assign wfull = ((wptr[DEPTH-1:0]) == w_rptr[DEPTH-1:0]) &&
+                    (wptr[DEPTH] != w_rptr[DEPTH]);
+    always @( *) begin
+        if(wptr[DEPTH] != w_rptr[DEPTH])
+            wgap = w_rptr[DEPTH-1:0] - wptr[DEPTH-1:0];
+        else 
+            wgap = FIFO_DEEP + w_rptr - wptr;
+    end
+    always @(posedge wclk or negedge wrst_n) begin
+        if(!wrst_n)
+            wfull_almost <= 1'b0;
+        else if(wgap < GAP)
+            wfull_almost <= 1'b1;
+        else
+            wfull_almost <= 1'b0;
+    end
+    // ------------------rempty and rempty_almost-------------------- //
+    always @( *) begin
+        rgap = r_wptr - rptr;//! is it right?
+    end
+    always @(posedge rclk or negedge rrst_n) begin
+        if(!rrst_n)
+            rempty_almost <= 1'b0;
+        else if(rgap < GAP)
+            rempty_almost <= 1'b1;
+        else 
+            rempty_almost <= 1'b0;
+    end
+    assign rempty = (rgap == 0)||((rgap == 1)&&(rinc));
+endmodule //async_fifo
